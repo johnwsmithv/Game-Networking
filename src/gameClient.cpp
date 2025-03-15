c48e21237d7524312984438b7e0e24df3e22b6dd
John William Smith V <jwsv61099@gmail.com>
2025-03-14 22:00:05 -0400

    Updated to use C++20, wanted "starts_with". Also, have a simple client-server setup going. Able to have the client connect to a server and have the server send back a UUID and some basic player information.

--- src/gameClient.cpp
+++ src/gameClient.cpp
@@ -1,5 +1,144 @@
 #include <iostream>
+#include <memory> // std::unique_ptr
+#include <ostream>
+
+#include <strings.h>    // bzero
+#include <sys/socket.h> // sockaddr_in on Linux
+#include <arpa/inet.h>  // Also needed for sockaddr works...
+#include <errno.h>      // For errno
+#include <unistd.h>     // STDIN_FILENO
+#include <cstring>      //memset
+
+#include <nlohmann/json.hpp>
+
+#include "Player.hpp"
+
+using json = nlohmann::json;
+
+struct Client {
+    int fd;
+    struct sockaddr_in server;
+
+    std::string uuid;
+    Player player;
+
+    Client()
+        : player(Player(0, Location(0, 0, 0)))
+    {}
+};
+
+std::ostream &operator<<(std::ostream &os, const std::unique_ptr<Client> &client) {
+    os << "UUID: " << client->uuid
+       << "\tPlayer(Health: " << client->player.getPlayerHealth()
+       << ", Location(" << client->player.getPlayerLocation().x
+       << ", " << client->player.getPlayerLocation().y
+       << ", " << client->player.getPlayerLocation().z
+       << ")\n";
+    return os;
+}
+
+// I am the client!!!
+std::unique_ptr<Client> client = nullptr;
+
+void sendToServer(std::unique_ptr<Client> &client, const std::string &message) {
+    sendto(client->fd, message.c_str(), message.size(), 0, (struct sockaddr *)&(client->server), sizeof(client->server));
+}
+
+void connectToServer(std::unique_ptr<Client> &client, const std::string &ip, const int portNum) {
+    client->fd = socket(PF_INET, SOCK_DGRAM, 0);
+    if (client->fd < 0) {
+        std::cerr << "Cannot open socket (" + errno << ")...\r\n";
+    }
+
+    bzero(&client->server, sizeof(client->server));
+    client->server.sin_family = AF_INET;
+    client->server.sin_port = htons(portNum);
+
+    inet_pton(AF_INET, ip.c_str(), &(client->server.sin_addr));
+}
 
 int main() {
-    std::cout << "Hello there client!\n";
+    // TODO: Change to not be hard coded...
+    constexpr int serverPort = 3000;
+    const std::string serverIp = "127.0.0.1";
+
+    client = std::make_unique<Client>();
+
+    // Connecting the client to the server!
+    connectToServer(client, serverIp, serverPort);
+
+    while (true) {
+        std::string clientInput = "";
+
+        fd_set fileDescSet;
+        int maxFd, ready = 0;
+
+        FD_ZERO(&fileDescSet);
+        FD_SET(STDIN_FILENO, &fileDescSet);
+        FD_SET(client->fd, &fileDescSet);
+
+        maxFd = STDIN_FILENO > client->fd ? STDIN_FILENO : client->fd;
+
+        // TODO: Add a timeout????
+        ready = select(maxFd + 1, &fileDescSet, NULL, NULL, NULL);
+        if (ready == -1) {
+            perror("Error with select...\n");
+            exit(EXIT_FAILURE);
+        } else if (ready == 0) {
+            // If we implement timout...
+            std::cerr << "Ready is 0...\n";
+        } else {
+            if (FD_ISSET(STDIN_FILENO, &fileDescSet)) {
+                // We want to get the clients input
+                std::getline(std::cin, clientInput);
+                sendToServer(client, clientInput);
+
+                // Receive response from Server...
+                char buf[1024];
+                memset(&buf, 0, sizeof(buf));
+                struct sockaddr_in src;
+                socklen_t srcSize = sizeof(src);
+                int rlen = recvfrom(client->fd, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&src, &srcSize);
+
+                // Always check the return value!
+                if (rlen == -1) {
+                    std::cerr << "Something went wrong: " << strerror(errno) << "...\n";
+                }
+
+                if (clientInput.starts_with("/newPlayer")) {
+                    json initialState = json::parse(buf);
+
+                    client->uuid = initialState["UUID"];
+                    client->player.updatePlayerHealth(initialState["Health"]);
+                    client->player.updatePlayerXLocation(initialState["Location"]["x"]);
+                    client->player.updatePlayerYLocation(initialState["Location"]["y"]);
+                    client->player.updatePlayerZLocation(initialState["Location"]["z"]);
+
+                    std::cout << "Updated player state:\n"
+                              << client << "\n";
+                }
+
+                if (std::string(buf).compare("+OK Client QUIT") == 0) {
+                    std::cout << "Client: Closing connection with Server...\n";
+                    break;
+                }
+
+                std::cout << buf << "\n";
+            }
+
+            if (FD_ISSET(client->fd, &fileDescSet)) {
+                char buf[1024];
+                struct sockaddr_in src;
+                socklen_t srcSize = sizeof(src);
+                int rlen = recvfrom(client->fd, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&src, &srcSize);
+                buf[rlen] = 0;
+
+                std::cout << buf << "\n";
+            }
+        }
+    }
+
+    close(client->fd);
+
+    return EXIT_SUCCESS;
 }
--- src/gameServer.cpp
+++ src/gameServer.cpp
@@ -4,15 +4,28 @@
 #include <random> // std::random_device, std::mt19937, std::uniform_int_distribution
 #include <sstream> // std::stringstream
 #include <iomanip> // std::setw, std::setfill
+#include <cstring> //memset
 
 #include <strings.h> // bzero
 #include <sys/socket.h> // sockaddr_in on Linux
 #include <arpa/inet.h> // Also needed for sockaddr works...
+#include <errno.h> // For errno
+
+#include <nlohmann/json.hpp>
 
 #include "Player.hpp"
 
+using json = nlohmann::json;
+
 struct Client {
     struct sockaddr_in destination;
+    std::string uuid;
+
+    Player player;
+
+    Client()
+        : player(Player(100, Location(0, 0, 0)))
+    {}
 };
 
 struct Server {
@@ -28,6 +41,9 @@ struct Server {
     // This is going to be used to write to when we send out 
     // packets to other clients of servers
     int serverFd;
+
+    // Clients IP + Port, Client struct
+    std::unordered_map<std::string, Client> clients;
 };
 
 /**
@@ -51,6 +67,17 @@ std::string generateUUID() {
        << std::setw(12) << dis(gen);
 
     return ss.str();
+}
+
+/**
+ * @brief Wrapper to the "sendto" function. This allows us to communicate to the Client.
+ * 
+ * @param serverFd The file descriptor of the socket which the server is going to be sending this data from.
+ * @param client The client information which we need to send this message to.
+ * @param message The message which we want to send.
+ */
+void sendToClient(int serverFd, const Client& client, const std::string& message) {
+    sendto(serverFd, message.c_str(), message.size(), 0, (struct sockaddr *)&(client.destination), sizeof(client.destination));
 }
 
 // The key is going to be the players id which is going to come from the client,
@@ -80,4 +107,64 @@ int main() {
         << " for any incoming connections....\n";
     // Actually bind the file descriptor to a local address
     bind(thisServer.serverFd, (struct sockaddr*)&servAddr, sizeof(servAddr));
+
+    // Infinite loop to listen for connections and the sorts...
+    while(true) {
+        struct sockaddr_in destination;
+        socklen_t clientSrcLen = sizeof(destination);
+
+        // The assignment specifies that the max we are going to receive is 1000
+        // chars, but hey, let's make it a power of two!
+        char buf[1024];
+        memset(&buf, 0, sizeof(buf));
+
+        // If I'm not mistaken, this is a blocking function and will only proceed once it receives
+        // a connection
+        const int rlen = recvfrom(
+            thisServer.serverFd, buf, sizeof(buf) - 1, 0, 
+            (struct sockaddr*)&destination, &clientSrcLen
+        );
+
+        // Always check the return value!
+        if(rlen == -1) {
+            std::cerr << "Something went wrong: " << strerror(errno) << "...\n";
+        }
+
+        std::string receivedMsg(buf);
+
+        // Determine who sent the message, get their IP Address and Port #
+        const std::string asciiIp(inet_ntoa(destination.sin_addr));
+        const int fixedPort = ntohs(destination.sin_port);
+        const std::string senderIp(asciiIp + ":" + std::to_string(fixedPort));
+
+        // If we have not seen this client before, add them to the list...
+        bool newClient = false;
+        if(thisServer.clients.find(senderIp) == thisServer.clients.end()) {
+            newClient = true;
+            Client newClient;
+            newClient.destination = destination;
+            thisServer.clients[senderIp] = newClient;
+        }
+        Client& client = thisServer.clients[senderIp];
+
+        std::cout << "Server: We received the following from the client => '" 
+            << receivedMsg << "'\n";
+
+        if(newClient) {
+            // We want to create them a UUID.
+            client.uuid = generateUUID();
+
+            json initalJoin;
+            initalJoin["UUID"] = client.uuid;
+            initalJoin["Health"] = client.player.getPlayerHealth();
+            initalJoin["Location"] = {};
+            initalJoin["Location"]["x"] = client.player.getPlayerLocation().x;
+            initalJoin["Location"]["y"] = client.player.getPlayerLocation().y;
+            initalJoin["Location"]["z"] = client.player.getPlayerLocation().z;
+
+            sendToClient(thisServer.serverFd, client, initalJoin.dump());
+        } else {
+            std::cout << "To be implemented...\n";
+        }
+    }
 }
